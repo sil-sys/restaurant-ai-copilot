2d52a5e698cb10140369f9b7303adb3fe9e999a4
sil-sys <silshah0801@gmail.com>
2026-03-05 17:09:55 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from revenue_engine import analyze_menu
-
-data_path = "data/restaurant_data.csv"
-
-df = analyze_menu(data_path)
-
-print("\nMenu Engineering Analysis:\n")
-print(df[["item","margin","quantity","menu_category"]])
